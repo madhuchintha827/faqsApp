2b22f210ca8bcf40e410bd0766dff8801be5358e
madhuchintha827 <chinthamadhu827@gmail.com>
2022-12-16 14:26:41 +0000

    Solution

---
+++ src/components/FaqItem/index.js
@@ -0,0 +1,53 @@
+// Write your code here.
+import {Component} from 'react'
+import './index.css'
+
+class FaqsItem extends Component {
+  state = {
+    isActiveQuestion: false,
+  }
+
+  onClickAnswer = () => {
+    this.setState(prevState => ({
+      isActiveQuestion: !prevState.isActiveQuestion,
+    }))
+  }
+
+  showAnswer = () => {
+    const {each} = this.props
+    const {answerText} = each
+    return (
+      <div className="answer-container">
+        <hr className="line" />
+        <p className="answer-para">{answerText}</p>
+      </div>
+    )
+  }
+
+  render() {
+    const {isActiveQuestion} = this.state
+    const {each} = this.props
+    const {questionText} = each
+    const image = isActiveQuestion
+      ? 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
+      : 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
+    const imageAlt = isActiveQuestion ? 'minus' : 'plus'
+    return (
+      <li className="container-list-items">
+        <div className="faq-question-container">
+          <h1 className="question">{questionText}</h1>
+          <button
+            onClick={this.onClickAnswer}
+            type="button"
+            className="plus-minus-btn"
+          >
+            <img src={image} alt={imageAlt} />
+          </button>
+        </div>
+
+        {isActiveQuestion && this.showAnswer()}
+      </li>
+    )
+  }
+}
+export default FaqsItem
---
+++ src/components/Faqs/index.js
@@ -0,0 +1,20 @@
+// Write your code here.
+import './index.css'
+import FaqItem from '../FaqItem'
+
+const Faqs = props => {
+  const {faqsList} = props
+  return (
+    <div className="container">
+      <div className="card-container">
+        <h1 className="heading">FAQS</h1>
+        <ul className="faqs-list-container">
+          {faqsList.map(each => (
+            <FaqItem each={each} key={each.id} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+export default Faqs
